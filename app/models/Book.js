cbe8211f98f58682d0b5b8207e377a3acba2d9c1
ariefhk <ariefrachmanhakim1001@gmail.com>
2023-04-01 22:11:33 +0700

    add models

--- app/models/Book.js
+++ app/models/Book.js
@@ -6,6 +6,11 @@ class Book {
   constructor(params) {
     this.id = this.#generateId();
     this.title = params.title;
+    this.coverImage = params.coverImage;
+    this.synopsis = params.synopsis;
+    this.publisher = params.publisher;
+    this.author = params.author;
+    this.price = params.price;
   }
 
   #generateId = () => {
@@ -13,6 +18,27 @@ class Book {
       this.constructor.records[this.constructor.records.length - 1]?.id || 0;
 
     return lastRecordId + 1;
+  };
+
+  update = (params) => {
+    const idx = this.constructor.records.findIndex((i) => i.id === this.id);
+
+    params.title && (this.title = params.title);
+    params.coverImage && (this.coverImage = params.coverImage);
+    params.synopsis && (this.synopsis = params.synopsis);
+    params.publisher && (this.publisher = params.publisher);
+    params.author && (this.author = params.author);
+    params.price && (this.price = params.price);
+
+    this.constructor.records[idx] = this;
+
+    return this;
+  };
+
+  delete = () => {
+    this.constructor.records = this.constructor.records.filter(
+      (i) => i.id !== this.id
+    );
   };
 
   static create = (params) => {
@@ -33,8 +59,3 @@ class Book {
     return this.records;
   };
 }
-
-Book.create({ title: "Hehe" });
-Book.create({ title: "Aslinya" });
-Book.create({ title: "Hahihu" });
-console.log(Book.list());
